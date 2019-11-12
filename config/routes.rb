578e4022aa31ed3715c055e1ecc1945abe5384b5
Nicola Tournay <nicolatournay@outlook.com>
2019-11-12 17:26:00 +0100

    added dropdown items and world nav-items

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'timelines/index'
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
