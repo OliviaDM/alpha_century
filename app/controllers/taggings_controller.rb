996f09847ab5d7ecd8fba02816360c9e921c197b
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-12 08:40:34 -0800

    tagging removal

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -4,7 +4,8 @@ class TaggingsController < ApplicationController
     tagname = params[:other][:name].strip.downcase
     tag = Tag.find_by(name: tagname)
     if tag
-      tagging = Tagging.create!(tag_id: tag.id, card_id: params[:card_id])
+      tagging = Tagging.new(tag_id: tag.id, card_id: params[:card_id])
+      tagging.save
     else
       tag = Tag.create!(world_id: params[:world_id], name: tagname)
       tagging = Tagging.create!(tag_id: tag.id, card_id: params[:card_id])
@@ -13,5 +14,8 @@ class TaggingsController < ApplicationController
   end
 
   def destroy
+    @tagging = Tagging.find(params[:id])
+    @tagging.destroy
+    redirect_to edit_world_card_path(world_id: params[:world_id], id: params[:card_id])
   end
 end
