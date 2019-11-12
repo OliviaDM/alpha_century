e9460b71f9ddd20c85b118d91816a0f0f598b974
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-12 01:41:54 -0800

    card index done

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,15 +1,16 @@
 class WorldsController < ApplicationController
+  before_action :set_world, only: [:edit, :update, :destroy]
+
   def index
     @worlds = World.all
   end
 
-  def show
-  end
-
   def new
+    @world = World.new
   end
 
   def create
+    @world = World.new(world_params)
   end
 
   def edit
@@ -19,5 +20,17 @@ class WorldsController < ApplicationController
   end
 
   def destroy
+    @world.destroy
+    redirect_to worlds_path
+  end
+
+  private
+
+  def set_world
+    @world = World.find(params[:id])
+  end
+
+  def world_params
+    params.require(:world).permit(:name, :description, :image)
   end
 end
