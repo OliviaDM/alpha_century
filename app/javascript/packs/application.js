ca05cefe87d4696de44848dd86e10665b492a2fd
corentin dawans <dawans.corentin@gmail.com>
2019-11-13 17:26:57 +0100

    remove things in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,1 @@
 import "bootstrap";
-  document.querySelector("#button-add-new-tag").addEventListener("click", (event) => {
-      event.preventDefault()
-      document.querySelector("#add-tag-form").classList.toggle('hide-div')
-      document.querySelector("#button-search-new-tag").classList.toggle('hide-div')
-      document.querySelector("#search-tag-form").classList.add('hide-div')
-      document.querySelector("#button-add-new-tag").classList.add('hide-div')
-
-  });
-
-  document.querySelector("#button-search-new-tag").addEventListener("click", (event) => {
-      event.preventDefault()
-      document.querySelector("#add-tag-form").classList.toggle('hide-div')
-      document.querySelector("#button-search-new-tag").classList.toggle('hide-div')
-      document.querySelector("#search-tag-form").classList.remove('hide-div')
-      document.querySelector("#button-add-new-tag").classList.remove('hide-div')
-
-  });
