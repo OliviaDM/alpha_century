d4234b03d463d9b36228aa5bdf780f4fc663f25a
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-13 08:35:33 -0800

    IT FUCKING WORKS MAN

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,7 @@
 class TimelinesController < ApplicationController
   def index
     @string = "hohohohoh"
-    @events = Card.events_tag_search(["wonky timeline"])
-    @hash = Timeline.new(["wonky timeline"]).json_ify
+    @events = Card.events_tag_search(["selected events"])
+    @hash = Timeline.new(["selected events"]).json_ify
   end
 end
--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -6,7 +6,7 @@ class Timeline
 
   def json_ify
     events = @events.map { |ev| { name: ev.title, id: ev.id } }
-    links = @links.map { |link| { source: @events.find_index(link.parent_event), target: @events.find_index(link.child_event) } }
+    links = relevent_links
     { nodes: events, edges: links }.to_json
   end
 
@@ -18,22 +18,22 @@ class Timeline
     until start_links.empty?
       edge = start_links.delete_at(0)
 
-      unless (edge.source == edge.target)
-        if @events.include?(edge.source) && @events.include?(edge.target)
+      unless edge[:source] == edge[:target]
+        if @events.include?(edge[:source]) && @events.include?(edge[:target])
 
           good_links.push(timelink_to_edge(edge))
 
-        elsif @event.include?(edge.source)
+        elsif @events.include?(edge[:source])
 
-          new_children = start_links.select { |e| e.source == edge.target }
-          new_edges = new_children.map { |e| { source: edge.source, target: e.target } }
-          start_links = start_links.reject { |e| e.source == edge.target } + new_edges
+          new_children = start_links.select { |e| e[:source] == edge[:target] }
+          new_edges = new_children.map { |e| { source: edge[:source], target: e[:target] } }
+          start_links = start_links.reject { |e| e[:source] == edge[:target] } + new_edges
 
-        elsif @event.include?(edge.target)
+        elsif @events.include?(edge[:target])
 
-          new_parents = start_links.select { |e| e.target == edge.source }
-          new_edges = new_parents.map { |e| { source: e.source, target: edge.target } }
-          start_links = start_links.reject { |e| e.target == edge.source } + new_edges
+          new_parents = start_links.select { |e| e[:target] == edge[:source] }
+          new_edges = new_parents.map { |e| { source: e[:source], target: edge[:target] } }
+          start_links = start_links.reject { |e| e[:target] == edge[:source] } + new_edges
 
         else
 
@@ -51,19 +51,23 @@ class Timeline
   private
 
   def timelink_to_edge(timelink)
-    { source: @event.find_index(timelink.source), target: @event.find_index(timelink.target) }
+    { source: @events.find_index(timelink[:source]), target: @events.find_index(timelink[:target]) }
   end
 
   def merge_useless_nodes(dummy, edge, array)
-    a = array.select { |e| e.source == edge.source || e.source == edge.target }
-    b = array.reject { |e| e.source == edge.source || e.source == edge.target }
-    a.each do { |e| e.source = dummy }
+    a = array.select { |e| e[:source] == edge[:source] || e[:source] == edge[:target] }
+    b = array.reject { |e| e[:source] == edge[:source] || e[:source] == edge[:target] }
+    a.each do |e|
+      e[:source] = dummy
+    end
 
     c = b + a
 
-    c = array.select { |e| e.target == edge.source || e.target == edge.target }
-    d = array.reject { |e| e.target == edge.source || e.target == edge.target }
-    c.each do { |e| e.target = dummy }
+    c = array.select { |e| e[:target] == edge[:source] || e[:target] == edge[:target] }
+    d = array.reject { |e| e[:target] == edge[:source] || e[:target] == edge[:target] }
+    c.each do |e|
+      e[:target] = dummy
+    end
 
     d + c
   end
