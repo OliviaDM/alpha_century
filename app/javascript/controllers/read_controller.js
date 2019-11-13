0567a327b8ea36bbe03e0ad3e0311b1d1520a14b
Arnaut Van de Perre <arnautvdp@gmail.com>
2019-11-13 16:26:42 +0100

    Implemented read more/less button

---
+++ app/javascript/controllers/read_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets =["dots", "more"]
+
+  initialize() {
+    console.log("The read controller is now loaded!");
+  }
+
+  readMore(e) {
+    const dots = this.dotsTarget
+    const more = this.moreTarget
+    const myBtn = e.currentTarget
+
+    console.log(dots)
+    console.log(more)
+    console.log(myBtn)
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      myBtn.innerHTML = "Read more";
+      more.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      myBtn.innerHTML = "Read less";
+      more.style.display = "inline";
+    }
+
+
+  }
+}
