8907f317a8c54289740f2510832534066c58becb
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-14 02:53:40 -0800

    display the right map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,11 @@
 class MapsController < ApplicationController
   def index
+    ind = Map.all.where(world_id: params[:world_id])
+    maps = []
+    ind.each do |map|
+      maps.push(map.attributes)
+    end
+    @map_hash = { maps: maps }
   end
 
   def show
---
+++ app/javascript/map_display.js
@@ -0,0 +1,25 @@
+function map_display() {
+  const dropdown = document.querySelector("#map-select");
+  const maps_info = JSON.parse(document.querySelector('#map').dataset.hash);
+  const map = document.querySelector('#map');
+  let current_map = "-1";
+
+  function find_map(id) {
+    return maps_info.maps.find(e => e.id.toString() == id);
+  }
+
+  function change_map(id) {
+    current_map = id;
+    const new_map = find_map(id);
+    map.innerHTML = `<img height="300" width="400" src="${new_map.image_url}">`;
+  };
+
+  dropdown.addEventListener("click", (event) => {
+    console.log(typeof(dropdown.value));
+    if (dropdown.value != "-1" && dropdown.value !=current_map ) {
+      change_map(dropdown.value);
+    }
+  });
+};
+
+export { map_display };
