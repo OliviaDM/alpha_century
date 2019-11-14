c16d7982fa69db200d92d958cd47efa00e6558ba
corentin dawans <dawans.corentin@gmail.com>
2019-11-14 12:31:56 +0100

    Trying stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import "./tag_click";
+import $ from 'jquery';
+import 'select2';
 
 import { draw_graph } from '../timeline_graph'
 
@@ -13,5 +15,11 @@ import { definitionsFromContext } from "stimulus/webpack-helpers";
 const application = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
+
+const cities = ["Amsterdam","Bali","Barcelona","Belo Horizonte","Berlin","Bordeaux","Brussels","Buenos Aires","Casablanca","Chengdu","Copenhagen","Kyoto","Lausanne","Lille","Lisbon","London","Lyon","Madrid","Marseille","Melbourne","Mexico","Milan","Montréal","Nantes","Oslo","Paris","Rio de Janeiro","Rennes","Rome","São Paulo","Seoul","Shanghai","Shenzhen","Singapore","Stockholm","Tel Aviv","Tokyo"];
+
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2({ data: cities });
+});
 
 
