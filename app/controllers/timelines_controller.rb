879efe0bad47c09c1ce13f38ea4f138a627d4cba
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-14 00:45:39 -0800

    missing column resolved

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,5 @@
 class TimelinesController < ApplicationController
   def index
-    @string = "hohohohoh"
-    @events = Card.events_tag_search(["selected events"])
     @hash = Timeline.new(["selected events"]).json_ify
   end
 end
