fb56a0743dcf7f88eaf66ed7a25d53cba86ecf39
corentin dawans <dawans.corentin@gmail.com>
2019-11-15 10:18:49 +0100

    fixed card creation

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -22,8 +22,9 @@ class CardsController < ApplicationController
   def create
     @card = Card.new(card_params)
     @card.world_id = params[:world_id]
+    @card.content = " " if @card.content.nil?
     @card.save
-    redirect_to world_cards_path(params[:world_id])
+    redirect_to edit_world_card_path(world_id: @card.world.id, id: @card.id)
   end
 
   def edit
