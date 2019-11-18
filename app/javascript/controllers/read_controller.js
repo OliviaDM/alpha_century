305610451665bf386e1e531c982700eb55e4e1bb
Arnaut Van de Perre <arnautvdp@gmail.com>
2019-11-18 23:42:30 +0100

    new readmore

--- app/javascript/controllers/read_controller.js
+++ app/javascript/controllers/read_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus";
+import $ from 'jquery'
 
 export default class extends Controller {
   static targets =["dots", "more"]
@@ -7,18 +8,22 @@ export default class extends Controller {
   }
 
   readMore(e) {
-    const dots = this.dotsTarget
-    const more = this.moreTarget
-    const myBtn = e.currentTarget
+    // const dots = this.dotsTarget
+    // const more = this.moreTarget
+    // const myBtn = e.currentTarget
 
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      myBtn.innerHTML = "Read more";
-      more.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      myBtn.innerHTML = "Read less";
-      more.style.display = "inline";
-    }
+    // if (dots.style.display === "none") {
+    //   dots.style.display = "inline";
+    //   myBtn.innerHTML = "Read more";
+    //   more.style.display = "none";
+    // } else {
+    //   dots.style.display = "none";
+    //   myBtn.innerHTML = "Read less";
+    //   more.style.display = "inline";
+    // }
+
+    $('#modal').on('shown.bs.modal', function () {
+      $('#myInput').trigger('focus')
+    })
   }
 }
