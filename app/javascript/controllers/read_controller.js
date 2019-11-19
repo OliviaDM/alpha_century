2b48d7d6d3183fa92ddcba4a1235a9b92351ddb7
Arnaut Van de Perre <arnautvdp@gmail.com>
2019-11-19 11:46:03 +0100

    resolved conflict

--- app/javascript/controllers/read_controller.js
+++ app/javascript/controllers/read_controller.js
@@ -12,23 +12,23 @@ export default class extends Controller {
     $('#modal').on('shown.bs.modal', function () {
       $('#myInput').trigger('focus')
     })
-    const dots = this.dotsTarget
-    const more = this.moreTarget
-    const less = this.lessTarget
-    const myBtn = e.currentTarget
+//     const dots = this.dotsTarget
+//     const more = this.moreTarget
+//     const less = this.lessTarget
+//     const myBtn = e.currentTarget
 
-    console.log(less);
+//     console.log(less);
 
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      myBtn.innerHTML = "Read more";
-      less.style.display = "inline";
-      more.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      myBtn.innerHTML = "Read less";
-      less.style.display = "none"
-      more.style.display = "inline";
-    }
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       myBtn.innerHTML = "Read more";
+//       less.style.display = "inline";
+//       more.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       myBtn.innerHTML = "Read less";
+//       less.style.display = "none"
+//       more.style.display = "inline";
+//     }
   }
 }
