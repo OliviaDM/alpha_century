7ce5fd507b94ccbae3bf19afbd264ee7abc6a7e1
Olivia De Meyere <oliviademeyere@gmail.com>
2019-11-20 05:52:49 -0800

    start branch

--- app/javascript/timeline_graph.js
+++ app/javascript/timeline_graph.js
@@ -1,169 +1,174 @@
 //import * as d3 from "d3";
 function draw_graph() {
 
-      let dataset = JSON.parse(document.querySelector('#timeline').dataset.string);
-      let select1 = null;
-      let select2 = null;
-      const select1_col = 'rgb(12,220,245)';
-      const select2_col = 'rgb(12,250,200)';
-      const unselect_col = "rgb(198, 45, 205)";
+  let dataset = JSON.parse(document.querySelector('#timeline').dataset.string);
+  let select1 = null;
+  let select2 = null;
+  const select1_col = 'rgb(12,220,245)';
+  const select2_col = 'rgb(12,250,200)';
+  const unselect_col = "rgb(198, 45, 205)";
+  let i = 0;
+  let j = 0;
 
 
-      function display_btn(from, to) {
-        const btn = document.querySelector('#create_link_btn');
-        const d_btn = document.querySelector('#delete_link_btn');
-        const from_id = parseInt(from.id.substring(4),10);
-        const to_id = parseInt(to.id.substring(4),10);
+  function display_btn(from, to) {
+    const btn = document.querySelector('#create_link_btn');
+    const d_btn = document.querySelector('#delete_link_btn');
+    const from_id = parseInt(from.id.substring(4),10);
+    const to_id = parseInt(to.id.substring(4),10);
+
+    btn.innerHTML = `Create a link from <strong>${dataset.nodes[from_id].name}</strong> to <strong>${dataset.nodes[to_id].name}</strong>`;
+    btn.style.display = "inline";
+
+    d_btn.innerHTML = `Delete a link from <strong>${dataset.nodes[from_id].name}</strong> to <strong>${dataset.nodes[to_id].name}</strong>`;
+    d_btn.style.display = "inline";
+
+    const token = $('meta[name=csrf-token]').attr('content');
+
+    d_btn.addEventListener("click", (event) => {
+      const fd = new FormData();
+      const link_info = JSON.stringify({ from: dataset.nodes[from_id].id, to: dataset.nodes[to_id].id });
+      fd.append('timelink', link_info);
 
 
-        btn.innerHTML = `Create a link from <strong>${dataset.nodes[from_id].name}</strong> to <strong>${dataset.nodes[to_id].name}</strong>`;
-        btn.style.display = "inline";
+      fetch('/timelinks/destroy',
+      {
+        method: "POST",
+        body: fd,
+        headers: {
+          'X-CSRF-Token': token
+        },
+        credentials: 'same-origin'
+      })
+        .then(response => response.json())
+        .then(data => {
+          // console.log(from_id);
+          if (data) {
+            const edgy = dataset.edges.find( e => e.target.index == to_id && e.source.index == from_id);
+            const ind = dataset.edges.indexOf(edgy);
+            dataset.edges.splice(ind, 1);
 
-        d_btn.innerHTML = `Delete a link from <strong>${dataset.nodes[from_id].name}</strong> to <strong>${dataset.nodes[to_id].name}</strong>`;
-        d_btn.style.display = "inline";
+            force
+              .nodes(dataset.nodes)
+              .links(dataset.edges)
+              .start();
 
+            restart();
 
-        const token = $('meta[name=csrf-token]').attr('content');
-
-
-
-        d_btn.addEventListener("click", (event) => {
-          const fd = new FormData();
-          const link_info = JSON.stringify({ from: dataset.nodes[from_id].id, to: dataset.nodes[to_id].id });
-          fd.append('timelink', link_info);
-
-
-          fetch('/timelinks/destroy',
-            { method: "POST",
-              body: fd,
-              headers: {
-                'X-CSRF-Token': token
-              },
-              credentials: 'same-origin'
-            })
-          .then(response => response.json())
-          .then(data => {
-            if (data) {
-              const edgy = dataset.edges.find( e => e.target.index == to_id && e.source.index == from_id);
-              const ind = dataset.edges.indexOf(edgy);
-              dataset.edges.splice(ind, 1);
-
-              force
-                .nodes(dataset.nodes)
-                .links(dataset.edges)
-                .start();
-
-              restart();
-
-            };
-            });
+          };
+          // console.log(dataset.ed);
         });
+    });
 
-        btn.addEventListener("click", (event) => {
-          const fd = new FormData();
-          const link_info = JSON.stringify({ from: dataset.nodes[from_id].id, to: dataset.nodes[to_id].id });
-          fd.append('timelink', link_info);
+    btn.addEventListener("click", (event) => {
+      const fd = new FormData();
+      const link_info = JSON.stringify({ from: dataset.nodes[from_id].id, to: dataset.nodes[to_id].id });
+      fd.append('timelink', link_info);
 
 
-          fetch('/timelinks/create',
-            { method: "POST",
-              body: fd,
-              headers: {
-                'X-CSRF-Token': token
-              },
-              credentials: 'same-origin'
-            })
-          .then(response => response.json())
-          .then(data => {
-            if (data.id) {
-              dataset.edges.push({target: to_id, source: from_id, original: true});
+      fetch('/timelinks/create',
+        { method: "POST",
+          body: fd,
+          headers: {
+            'X-CSRF-Token': token
+          },
+          credentials: 'same-origin'
+        })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(dataset.edges);
+        // console.log(from_id);
+        if (data.id) {
+          dataset.edges.push({target: to_id, source: from_id});
 
-              force
-                .nodes(dataset.nodes)
-                .links(dataset.edges)
-                .start();
+          force
+            .nodes(dataset.nodes)
+            .links(dataset.edges)
+            .start();
 
-              restart();
+          restart();
 
-            };
-            });
-
-        });
-      };
-
-      function undisplay_btn() {
-        document.querySelector('#create_link_btn').style.display = "none";
-        document.querySelector('#delete_link_btn').style.display = "none";
-      };
-
-      const name_toggle = document.querySelector('#name_toggle');
-      name_toggle.addEventListener("click", (event) => {
-        if (name_toggle.checked) {
-          nodelabels.attr({'display':'inline'});
-        } else {
-          nodelabels.attr({'display':'none'});
         };
+        // console.log(dataset.edges.map {e => e.id});
+        // console.log(dataset.edges);
       });
 
-      function restart() {
+    });
+  };
 
+  function undisplay_btn() {
+    document.querySelector('#create_link_btn').style.display = "none";
+    document.querySelector('#delete_link_btn').style.display = "none";
+  };
 
-        let edges = svg.selectAll("line")
-            .data(dataset.edges);
-
-        edges.enter()
-            .append("line")
-            .attr("id",function(d,i) {return 'edge'+i})
-            .attr('marker-end','url(#arrowhead)')
-            .attr("style", function(d) {
-              if (d.original) {
-                return ("stroke-width: 2px;");
-              } else {
-                return ("stroke-width: 1px;");
-              };
-            })
-            .style("stroke","#ccc")
-            .style("pointer-events", "none");
-
-        edges.exit().remove();
-
-        let edgepaths = svg.selectAll(".edgepath")
-          .data(dataset.edges);
-
-        edgepaths.enter()
-          .append('path')
-          .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
-                 'class':'edgepath',
-                 'fill-opacity':0,
-                 'stroke-opacity':0,
-                 'fill':'blue',
-                 'stroke':'red',
-                 'id':function(d,i) {return 'edgepath'+i}})
-          .style("pointer-events", "none");
-
-        edgepaths.exit().remove();
-
-      force.on("tick", function(){
-
-          edges.attr({"x1": function(d){return d.source.x;},
-                      "y1": function(d){return d.source.y;},
-                      "x2": function(d){return d.target.x;},
-                      "y2": function(d){return d.target.y;}
-          });
-
-          nodes.attr({"cx":function(d){return d.x;},
-                      "cy":function(d){return d.y;}
-          });
-
-          nodelabels.attr("x", function(d) { return d.x; })
-                    .attr("y", function(d) { return d.y; });
-
-          edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
-                                             //console.log(d)
-                                             return path});
-      });
-
+  const name_toggle = document.querySelector('#name_toggle');
+  name_toggle.addEventListener("click", (event) => {
+    if (name_toggle.checked) {
+      nodelabels.attr({'display':'inline'});
+    } else {
+      nodelabels.attr({'display':'none'});
     };
+  });
+
+  function restart() {
+
+
+    let edges = svg.selectAll("line")
+        .data(dataset.edges);
+
+    edges.enter()
+        .append("line")
+        .attr("id",function(d,i) {return 'edge'+i})
+        .attr('marker-end','url(#arrowhead)')
+        .attr("style", function(d) {
+          // if (d.original) {
+            // return ("stroke-width: 2px;");
+          // } else {
+            return ("stroke-width: 1px;");
+          // };
+        })
+        .style("stroke","#ccc")
+        .style("pointer-events", "none");
+
+    edges.exit().remove();
+
+    let edgepaths = svg.selectAll(".edgepath")
+      .data(dataset.edges);
+
+    edgepaths.enter()
+      .append('path')
+      .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
+             'class':'edgepath',
+             'fill-opacity':0,
+             'stroke-opacity':0,
+             'fill':'blue',
+             'stroke':'red',
+             'id':function(d,i) {return 'edgepath'+i}})
+      .style("pointer-events", "none");
+
+    edgepaths.exit().remove();
+
+    force.on("tick", function(){
+
+      edges.attr({"x1": function(d){return d.source.x;},
+                  "y1": function(d){return d.source.y;},
+                  "x2": function(d){return d.target.x;},
+                  "y2": function(d){return d.target.y;}
+      });
+
+      nodes.attr({"cx":function(d){return d.x;},
+                  "cy":function(d){return d.y;}
+      });
+
+      nodelabels.attr("x", function(d) { return d.x; })
+                .attr("y", function(d) { return d.y; });
+
+      edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+                                         //console.log(d)
+                                         return path});
+  });
+
+};
 
     const w = 1000, h = 600;
 
@@ -283,6 +288,7 @@ function draw_graph() {
               .attr('fill', '#ccc')
               .attr('stroke','#ccc');
 
+    // console.log(dataset.edges.map {e => e.id })
   };
 
 export { draw_graph };
